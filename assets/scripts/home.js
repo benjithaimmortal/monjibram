aa27856c6638e48462d7435f98c436c71917841b
Benjamin Kostenbader <benjamin.kostenbader@gmail.com>
2022-12-26 19:17:36 -0500

    testimonials block

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -20,4 +20,54 @@ $(function(){
       filterCards(filter, $(this));
     }
   })
+  $('.slider').slick({
+    arrows: false,
+    draggable: false,
+    initialSlide: 0,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    swipe: true,
+    pauseOnHover: true,
+    infinite: true,
+    autoplay: true,
+    fade: true,
+    asNavFor: '.author-slider, .img-slider'
+  })
+  $('.img-slider').slick({
+    arrows: false,
+    draggable: false,
+    initialSlide: 0,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    // swipe: true,
+    // pauseOnHover: true,
+    // infinite: true,
+    // autoplay: true,
+    fade: true,
+    asNavFor: '.author-slider, .slider'
+  })
+  $('.author-slider').slick({
+    arrows: false,
+    draggable: false,
+    initialSlide: 0,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    // swipe: true,
+    // pauseOnHover: true,
+    // infinite: true,
+    // autoplay: true,
+    asNavFor: '.slider, .img-slider',
+  })
+  
+  $('.slider, .img-slider, .author-slider').click(function(){
+    $('.slider').slick('slickNext');
+  })
+    // .hover(function(){
+    //   $('.author-slider').slick('slickPause');
+    // }, function(){
+    //   $('.author-slider').slick('slickPlay');
+    // });
 });
--- docs/README.md
+++ docs/README.md
@@ -1,6 +1,3 @@
 It would be really useful if this told you how to build my site, wouldn't it?
 
 [Contact me](https://www.monjibram.com/contact) if you want to learn more about that. I mentor!
-
-But really, to build this boi, use this line before pushing to GH
-`JEKYLL_ENV=production jekyll build`
